f1c8b92a78301f010a42633bd1c406d70f547795
Nguyen Duc Duy <jhvgame123@gmail.com>
2022-09-30 21:16:42 +0700

    Added Timer and Goal

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Player;
+    [SerializeField]
+    private GameObject startPoint;
+
+    Timer timer;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player.transform.position = new Vector2(startPoint.transform.position.x, Player.transform.position.y);
+            timer.PauseTimer();
+        }
+    }
+}
