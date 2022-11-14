ed6df017a06ab026f69ec32cf36af30c1d31ee46
Nguyen Duc Duy <jhvgame123@gmail.com>
2022-11-14 18:49:02 +0700

    Rework Stage1

--- Assets/Scripts/Environments/Tree.cs
+++ Assets/Scripts/Environments/Tree.cs
@@ -18,7 +18,7 @@ public class Tree : Watch
         StartCoroutine(Timer());
     }
 
-    public IEnumerator Timer()
+    IEnumerator Timer()
     {
         yield return new WaitForSeconds(1.5f);
 
