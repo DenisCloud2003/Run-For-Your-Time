024085d2d22953f0c3e4e675bbd7d431335d351e
Nguyen Duc Duy <jhvgame123@gmail.com>
2022-11-15 17:30:10 +0700

    zzz

--- Assets/Scripts/Items/Watch.cs
+++ Assets/Scripts/Items/Watch.cs
@@ -7,6 +7,8 @@ public class Watch : ItemBase
     [SerializeField] protected bool isForward;
     [SerializeField] protected bool isBackward;
 
+    [SerializeField] GameObject watchPrefab;
+
     public int count = 0;
     public float cooldownTimer;
 
