2b6435e8abc477bd20dde10418055d997d47336a
Nguyen Duc Duy <jhvgame123@gmail.com>
2022-11-22 00:41:10 +0700

    Reset Success

---
+++ Assets/Scripts/Environments/Tree.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : Watch
+{
+    private int animCount = 0;
+    Animator treeAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        treeAnim = GetComponent<Animator>();
+    }
+
+    public void CheckInput()
+    {
+        StartCoroutine(Timer());
+    }
+
+    public IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        if (isUsing)
+        {
+            if (isForward && !isBackward && animCount < 4)
+            {
+                treeAnim.SetTrigger("Forward");
+                animCount++;
+            }
+            else if (isBackward && !isForward && animCount > 0)
+            {
+                treeAnim.SetTrigger("Backward");
+                animCount--;
+            }
+            else StartCoroutine(Timer());
+
+            if (isForward && animCount == 4)
+            {
+                isUsing = false;
+            }
+        }
+        else
+        {
+            StartCoroutine(Timer());
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    PauseManager pauseManager;
+
+    private void Awake()
+    {
+        pauseManager = GetComponent<PauseManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (panel.activeInHierarchy == false)
+            {
+                OpenMenu();
+            }
+            else
+            {
+                CloseMenu();
+            }
+        }
+    }
+
+    public void OpenMenu()
+    {
+        pauseManager.PauseGame();
+        panel.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        pauseManager.UnPauseGame();
+        panel.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
